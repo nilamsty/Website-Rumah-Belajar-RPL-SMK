1080390104ddfc82a0777e66539046a8e69f0162
nilamsty <nilamsetyoningrum4@gmail.com>
2021-11-16 07:42:16 +0700

    cari data pengguna

--- resources/views/admin/data-pengguna/index.blade.php
+++ resources/views/admin/data-pengguna/index.blade.php
@@ -9,8 +9,18 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <h5>Cari Nama Pengguna! </h5>
+                    <form class="d-flex" method="GET" action="{{route('datapengguna')}}">
+                        <input name="cari" class="form-control me-2" type="search" placeholder="Masukkan Nama Pengguna" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Cari</button>
+                    </form>
                     
+                    <br>
+                    <br>
 
+                    <h5>Data Pengguna! <a href="{{route('datapengguna')}}" class="btn btn-primary btn-sm">
+                        <span class="fa fa-sync"></span>
+                    </a></h5> 
                     @if (Session::has('success'))
                         <div class="alert alert-success" role="alert">
                             {{Session::get('success')}}
